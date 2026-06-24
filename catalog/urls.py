9fd7f60571eee99b40ee0d314329dfb26fcc830c
Max <maxi4o@mail.ru>
2026-06-24 10:56:54 +0300

    finished

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from catalog import views
+
+urlpatterns = [
+    path('', views.home_page, name='home'),
+    path('home/', views.home_page, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+]
