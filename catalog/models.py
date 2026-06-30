53463f338c5b49d1904bc454c04678202f1f0a1a
Max <maxi4o@mail.ru>
2026-06-30 20:27:08 +0300

    added models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    class Meta:
+        pass
+
+class Category(models.Model):
+    class Meta:
+        pass
