5058703d6670c34035615852a148e577ee27f94f
Max <maxi4o@mail.ru>
2026-07-13 23:51:41 +0300

    added blog url paths

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from catalog import views
+from blog import views
 
 urlpatterns = [
-
+    path('/', views.ArticlesListView.as_view(), name='articles_list'),
+    path('/<int:pk>/', views.ArticlesDetailView.as_view(), name='articles_detail'),
+    path('/new/', views.ArticlesCreateView.as_view(), name='articles_create'),
+    path('/<int:pk>/edit/', views.ArticlesUpdateView.as_view(), name='articles_edit'),
+    path('/<int:pk>/delete/', views.ArticlesDeleteView.as_view(), name='articles_delete'),
 ]
