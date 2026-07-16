bca3544e0d6fec2e53271bfc8e9b91692542f324
Max <maxi4o@mail.ru>
2026-07-16 23:46:08 +0300

    fixed forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,48 +10,51 @@ class ProductForm(forms.ModelForm):
         model = models.Product
         fields = ['name', 'description', 'picture', 'category', 'price']
 
-        def clean_name(self):
-            name = self.cleaned_data.get('name')
-            for bad_word in bad_words:
-                if bad_word in name.lower():
-                    raise ValidationError('Имя не должно содержать запрещенных слов!')
-            return name
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        for bad_word in bad_words:
+            if bad_word in name.lower():
+                raise ValidationError('Имя не должно содержать запрещенных слов!')
+        return name
 
-        def clean_description(self):
-            description = self.cleaned_data.get('description')
-            for bad_word in bad_words:
-                if bad_word in description.lower():
-                    raise ValidationError('Описание не должно содержать запрещенных слов!')
-            return description
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        for bad_word in bad_words:
+            if bad_word in description.lower():
+                raise ValidationError('Описание не должно содержать запрещенных слов!')
+        return description
 
-        def clean_price(self):
-            price = self.cleaned_data.get('price')
-            if price <= 0:
-                raise ValidationError('Цена не может быть отрицательной, или нулевой')
-            return price
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price <= 0:
+            raise ValidationError('Цена не может быть отрицательной, или нулевой')
+        return price
 
-        def __init__(self, *args, **kwargs):
-            super(ProductForm, self).__init__(*args, **kwargs)
+    def clean(self):
+        cleaned_data = super().clean()
 
-            self.fields['name'].widget.attrs.update({
-                'class': 'form-control',
-                'placeholder': 'Введите название'
-            })
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
 
-            self.fields['description'].widget.attrs.update({
-                'class': 'form-control',
-                'placeholder': 'Введите описание'
-            })
+        self.fields['name'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название'
+        })
 
-            self.fields['price'].widget.attrs.update({
-                'class': 'form-control',
-                'placeholder': 'Введите цену'
-            })
-            self.fields['picture'].widget.attrs.update({
-                'class': 'form-control',
-                'placeholder': 'Вставьте картинку'
-            })
-            self.fields['category'].widget.attrs.update({
-                'class': 'form-control',
-                'placeholder': 'Вставьте категорию'
-            })
+        self.fields['description'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите описание'
+        })
+
+        self.fields['price'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите цену'
+        })
+        self.fields['picture'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Вставьте картинку'
+        })
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Вставьте категорию'
+        })
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
@@ -9,38 +11,48 @@ class ProductListView(ListView):
     template_name = 'home.html'
     context_object_name = 'product_list'
 
+
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_page.html'
     context_object_name = 'product'
 
+
 class ProductCreateView(CreateView):
     model = Product
-    fields = []
-    template_name = 'product_page.html'
+    form_class = ProductForm
+    template_name = 'product_new.html'
+    success_url = reverse_lazy('home')
+
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = []
+    form_class = ProductForm
     context_object_name = 'product'
-    template_name = 'product_page.html'
+    template_name = 'product_update_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('product', kwargs={'pk': self.object.pk})
+
 
 class ProductDeleteView(DeleteView):
     model = Product
-    template_name = 'product_page.html'
+    template_name = 'product_confirm_delete.html'
+    success_url = reverse_lazy('home')
+
 
 class ContactView(TemplateView):
     template_name = 'contacts.html'
-#def home_page(request):
+# def home_page(request):
 #    product_list = Product.objects.all()
 #    context = {'product_list': product_list}
 #    return render(request, "home.html", context)
 
 
-#def contacts(request):
+# def contacts(request):
 #    return render(request, "contacts.html")
 
-#def product_page(request, pk):
+# def product_page(request, pk):
 #    product_data = Product.objects.get(pk=pk)
 #    context = {'product': product_data}
 #    return render(request, "product_page.html", context)
