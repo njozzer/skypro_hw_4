49e682127b175cc24516779340378eb256f2b208
Max <maxi4o@mail.ru>
2026-07-21 14:41:06 +0300

    added pages and forms

--- custom_auth/forms.py
+++ custom_auth/forms.py
@@ -2,18 +2,41 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from custom_auth import models
 
+
 class CustomUserCreationForm(UserCreationForm):
-    phone_number = forms.CharField(max_length=15, required=False, help_text='Необязательное поле. Введите ваш номер телефона.')
+
 
     class Meta(UserCreationForm.Meta):
         model = models.CustomUser
-        fields = '__all__'
+        fields = ['email']
 
-    def clean_phone_number(self):
-        phone_number = self.cleaned_data.get('phone_number')
-        if phone_number and not phone_number.isdigit():
-            raise forms.ValidationError('Номер телефона должен содержать только цифры.')
-        return phone_number
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите название'
+        })
+        self.fields['password1'].widget = forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password'
+        })
+        self.fields['password2'].widget = forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirm Password'
+        })
+
 
 class CustomAuthenticationForm(AuthenticationForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите email'
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
+
--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from catalog import views
+from custom_auth import views
 
 urlpatterns = [
-    path("login/", views.ProductListView.as_view(), name="login"),
-    path("logout/", views.ProductListView.as_view(), name="logout"),
-    path("register/", views.ProductListView.as_view(), name="register"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+    path("register/", views.CustomRegisterView.as_view(), name="register"),
 ]
 
